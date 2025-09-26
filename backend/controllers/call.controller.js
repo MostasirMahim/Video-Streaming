3f2e6aa50106990bd6dcb7f42fa887dc45e1ffde
Mostasir Mahim <mahimlive@gmail.com>
2025-09-26 22:39:30 +0600

    perf(backend): added single 1-1 video call token & connection generation.

--- backend/controllers/call.controller.js
+++ backend/controllers/call.controller.js
@@ -1,31 +1,26 @@
 import { AccessToken } from "livekit-server-sdk";
 import User from "../models/user.model.js";
 
-const createToken = async(req, res) => {
+const createToken = async (req, res) => {
   const roomName = req.query.room;
-  console.log("Room Name:", roomName);
   const user = await User.findById(req.user._id);
-    if (!user || !roomName) {
-      return res.status(404).json({ error: "User Or Room not found" });
-    }
+  if (!user || !roomName) {
+    return res.status(404).json({ error: "User Or Room not found" });
+  }
 
   const participantName = user?.username;
-  console.log("Participant Name:", participantName);
-
   const at = new AccessToken(
     process.env.LIVEKIT_API_KEY,
     process.env.LIVEKIT_API_SECRET,
     {
       identity: participantName,
-      ttl: "10m", // expire after 10 minutes
+      ttl: "10m",
     }
   );
 
   at.addGrant({ roomJoin: true, room: roomName });
 
   const token = await at.toJwt();
-  console.log("Token:", token);
-
   res.json({ token });
 };
 
--- backend/server.js
+++ backend/server.js
@@ -7,13 +7,11 @@ import userRoutes from "./routes/user.routes.js";
 import conversationRoutes from "./routes/conversation.routes.js";
 import { app, server } from "./socket/socket.js";
 import { v2 as cloudinary } from "cloudinary";
-import path from "path";
 import groupsRoutes from './routes/group.routes.js'
 import callRoutes from './routes/call.routes.js'
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
-const __dirname = path.resolve();
 app.use(cookieparser());
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ limit: "5mb", extended: true }));
@@ -24,18 +22,13 @@ cloudinary.config({
   api_secret: process.env.API_SECRET,
 });
 
-
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/messages", conversationRoutes);
 app.use("/api/groups", groupsRoutes);
 app.use("/api/call", callRoutes);
 
-
-
-
 server.listen(PORT, () => {
-  console.log("Volume Test is ok");
   console.log(`server is running on ${PORT}`);
   connectMongoDb();
 });
